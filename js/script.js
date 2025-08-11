a3c3784b999e9a931fb6cbb6eb68c3d21acb5de5
Luis Fernando Paucar <lfernandopaucar28@gmail.com>
2025-08-10 21:32:37 -0500

    Se agrego movimiento a cuyo :D

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const mensaje =document.getElementById("mensaje");
+    const mensaje = document.getElementById("mensaje");
     mensaje.textContent = "¡Hola soy Cuyo! Tu nueva mascota";
+
+    let mascota = document.getElementById("mascota");
+    let x = 100; 
+    let y = 300;
+
+    let frame = 0;
+    let imagenes = [
+        "mascota-imagen/cuyo-parado.png",
+        "mascota-imagen/cuyo-caminando.png"
+    ];
+
+    setInterval(() => {
+        x += 5;
+        mascota.style.left = x + "px";
+
+        frame = (frame + 1) % imagenes.length;
+        mascota.src = imagenes[frame];
+    }, 100);
 });
